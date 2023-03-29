979564b15f43cac475af7ea55ceec5f9fd153bb1
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-03-29 23:46:47 +0100

    Set Up App, CustomForm, TaskList, TaskItem Components, plus additional modules.css for the last two

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// custom components
+import TaskItem from "./TaskItem";
+
+// styles
+import styles from "./TaskList.module.css"
+
+const TaskList = ({tasks}) => {
+  return (
+    <ul className={styles.tasks}>
+    {tasks.sort((a,b) => b.id - a.id).map(task => (
+        <TaskItem 
+         key={task.id}
+         task={task}   
+        />
+    ))
+    }
+    </ul>
+  )
+}
+
+export default TaskList
+ 
