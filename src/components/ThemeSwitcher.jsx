1b3a7683cb7a70147503c9488f8a9216c8621ac4
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-04-03 21:02:25 +0100

    Added the button and icons to ThemeSwitcher component

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
+
+// Icons
+import {
+  XMarkIcon,
+  SunIcon,
+  MoonIcon,
+  SwatchIcon,
+} from "@heroicons/react/24/outline";
+
+// Styles
+import styles from "./ThemeSwitcher.module.css";
 
 const ThemeSwitcher = () => {
-  return (
-    <div>
-      Theme Switcher
-    </div>
-  )
-}
+  const [isColorPicking, setIsColorPicking] = useState(false);
 
-export default ThemeSwitcher
+  return (
+    <aside
+      className={styles.wrapper}
+      // style
+    >
+      {isColorPicking ? (
+        <>
+          <button>a</button>
+          <input type="range" />
+        </>
+      ) : (
+        <div className={styles.btns}>
+          <button className="btn">
+            <MoonIcon />
+          </button>
+          <button className="btn">
+            <SwatchIcon />
+          </button>
+        </div>
+      )
+      }
+    </aside>
+  );
+};
+
+export default ThemeSwitcher;
